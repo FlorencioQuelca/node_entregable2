26142ece77c86f071385041746c10af56eb1c0da
FlorencioQuelca <florens.lonwe@gmail.com>
2022-11-20 22:43:03 -0400

    terminado

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -6,6 +6,6 @@ router.get('/users', userServices.getAllUsers)
 router.post('/users', userServices.postNewUser)
 router.get('/users/:id', userServices.getUserById)
 router.delete('/users/:id', userServices.deleteUserById)
-router.patch('/users/:id', userServices.editUserById)
+router.put('/users/:id', userServices.editUserById)
 
 module.exports = router
